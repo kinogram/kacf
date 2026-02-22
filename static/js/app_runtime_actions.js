6a144ee020e0eb033355a10f7ee6f5fe6b49459d
kevinboox <gregsons334@gmail.com>
2026-02-22 12:45:01 +0800

    refactor(ui): reduce novice project-management button noise

--- static/js/app_runtime_actions.js
+++ static/js/app_runtime_actions.js
@@ -577,6 +577,9 @@ function applyNoviceUi(me, isGuest) {
     const isAdmin = !!(me && me.is_admin);
     setHiddenById('advanced_git_section', true);
     setHiddenById('advanced_unattended_section', true);
+    setHiddenById('project_save_btn', true);
+    setHiddenById('project_load_btn', true);
+    setHiddenById('project_delete_btn', true);
     setHiddenById('vm_card', isGuest || !isAdmin);
 }
 
